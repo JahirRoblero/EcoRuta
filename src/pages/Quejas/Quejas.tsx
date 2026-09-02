375f1c85e8b8431967fdb0115fac95d22d9ba56f
DaniielA5 <juadaxis00@gmail.com>
2026-09-02 07:10:52 -0600

    subire quejas y sugerencias

---
+++ src/pages/Quejas/Quejas.tsx
@@ -0,0 +1,12 @@
+import "./Quejas.css"
+
+ function Quejas(){
+    return (
+        <div>
+            <h1>Quejas y Sugerencias</h1>
+            <p>Aqui ira el formulario</p>
+        </div>
+    );
+ }
+
+ export default Quejas;
